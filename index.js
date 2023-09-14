43cd6954ff9144f105aaf98e762e34246daa12e4
Jithinppp <jithinp968@gmail.com>
2023-09-14 15:35:02 +0530

    added env and port variable

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 // modules are used to share code or files across project
 // const express = require("express") this type of import called common js modules
 // import express from "express" is es2015 modules that is not supported in node
-// import express from "express";
 const express = require("express");
-// run the express to manage the req and res
+// run the express to manage the req and res initiating express server to manage http req
 const app = express();
 
-// getting the home route
+// getting the home route and responds
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
 
 // listen to a port
-app.listen(5000);
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
